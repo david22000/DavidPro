b82fe3d0792b791a20ba8421e7ccbb482f54fed5
david22000 <110238507+david22000@users.noreply.github.com>
2023-01-17 23:03:18 -0300

    Update index.js

--- Projeto Pokemon/src/js/index.js
+++ Projeto Pokemon/src/js/index.js
@@ -1,31 +1,4 @@
-/*
 
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostra o proximo cartão da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-    - passo 3 - fazer aparecer o proximo carta da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostra o anteior cartão da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-    - passo 3 - fazer aparecer o anterior carta da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-*/
-
-
-
-
-/*
-
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostra o proximo cartão da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-    - passo 3 - fazer aparecer o proximo carta da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-*/
-//  - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
 const btnAvancar = document.getElementById('btn-avancar');
 const btnVoltar = document.getElementById('btn-voltar');
 const cartao = document.querySelectorAll('.cartao');
